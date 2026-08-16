e3809292c2b91026a0b7da1f745d352a4eba0d7d
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-16 23:57:34 +0100

    fix: Use the metadata route robots type.

--- admin-website/src/app/robots.ts
+++ admin-website/src/app/robots.ts
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
+import type {MetadataRoute} from "next";
 
-export default function robots(): Metadata.Robots {
+export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
             userAgent: "*",
