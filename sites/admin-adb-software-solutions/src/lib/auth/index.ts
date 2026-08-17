22de4e5834df1071408e0d24afacea778749be5d
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-17 02:10:25 +0100

    refactor: Remove legacy TechWiki auth exports.

--- sites/admin-adb-software-solutions/src/lib/auth/index.ts
+++
@@ -1,9 +0,0 @@
-export { AuthProvider, useAuth, useCanEditArticle } from "./AuthContext";
-export {
-    canCreateArticle,
-    canDeleteArticle,
-    canEditArticle,
-    canModerate,
-    canPublishDirectly,
-} from "./types";
-export type { AuthState, WikiUser, WikiUserRole } from "./types";
