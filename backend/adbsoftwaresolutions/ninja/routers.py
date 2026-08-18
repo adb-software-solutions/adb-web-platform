0913d84bd11be1a85cfff7ca6dd4aaa2b6d02939
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-18 04:53:28 +0100

    feat: Register infrastructure admin routes.

--- backend/adbsoftwaresolutions/ninja/routers.py
+++ backend/adbsoftwaresolutions/ninja/routers.py
@@ -10,6 +10,7 @@ from pydantic import ValidationError
 from apps.clients.ninja.admin_views import clients_admin_router
 from apps.core.ninja.admin_views import core_admin_router
 from apps.crm.ninja.admin_views import crm_admin_router
+from apps.infrastructure.ninja.admin_views import infrastructure_admin_router
 from apps.tasks.ninja.admin_views import tasks_admin_router
 from apps.website.ninja.admin_views import website_admin_router
 from apps.website.ninja.views import website_misc_router, website_public_router
@@ -42,6 +43,7 @@ admin_router.add_router("", core_admin_router)
 admin_router.add_router("", clients_admin_router)
 admin_router.add_router("", crm_admin_router)
 admin_router.add_router("", tasks_admin_router)
+admin_router.add_router("", infrastructure_admin_router)
 admin_router.add_router("/website", website_admin_router)
 api.add_router("/admin", admin_router)
 
