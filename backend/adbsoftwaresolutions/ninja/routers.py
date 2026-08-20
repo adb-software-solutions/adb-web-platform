c20653bcd4838b5e88676c9ca73c1ed3dca8b735
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-20 01:29:41 +0100

    feat: Add personal task focus views.

--- backend/adbsoftwaresolutions/ninja/routers.py
+++ backend/adbsoftwaresolutions/ninja/routers.py
@@ -16,6 +16,7 @@ from apps.crm.ninja.admin_views import crm_admin_router
 from apps.infrastructure.ninja.admin_views import infrastructure_admin_router
 from apps.infrastructure.ninja.extended_views import infrastructure_extended_router
 from apps.tasks.ninja.admin_views import tasks_admin_router
+from apps.tasks.ninja.focus_views import focus_router
 from apps.tasks.ninja.relations_views import relations_router
 from apps.tasks.ninja.workspace_views import workspace_router
 from apps.ticketing.ninja.admin_views import ticketing_admin_router
@@ -56,6 +57,7 @@ admin_router.add_router("", time_tracking_router)
 admin_router.add_router("", time_report_router)
 admin_router.add_router("", crm_admin_router)
 admin_router.add_router("", tasks_admin_router)
+admin_router.add_router("", focus_router)
 admin_router.add_router("", workspace_router)
 admin_router.add_router("", relations_router)
 admin_router.add_router("", ticketing_admin_router)
