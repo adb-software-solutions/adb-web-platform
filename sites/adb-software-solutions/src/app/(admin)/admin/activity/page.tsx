41b620374f3c97ad2450c102a1c48ec08b9ad9be
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-28 03:39:35 +0100

    feat: Add operational activity page.

---
+++ sites/adb-software-solutions/src/app/(admin)/admin/activity/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui";
+import { ActivityWorkspace } from "./ActivityWorkspace";
+
+export default function ActivityPage() {
+    return (
+        <Container className="py-8">
+            <ActivityWorkspace />
+        </Container>
+    );
+}
