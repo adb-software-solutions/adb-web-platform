cd98ae49650d0a5099aa6d528e27b51c215a15d1
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-28 03:40:47 +0100

    feat: Add ticket service level page.

---
+++ sites/adb-software-solutions/src/app/(admin)/admin/service-levels/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui";
+import { ServiceLevelsWorkspace } from "./ServiceLevelsWorkspace";
+
+export default function ServiceLevelsPage() {
+    return (
+        <Container className="py-8">
+            <ServiceLevelsWorkspace />
+        </Container>
+    );
+}
