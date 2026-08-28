c69e0c221ec4d1ddac0d675dfb4e480e516502b5
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-28 03:41:30 +0100

    feat: Add infrastructure topology page.

---
+++ sites/adb-software-solutions/src/app/(admin)/admin/infrastructure/topology/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui";
+import { TopologyWorkspace } from "./TopologyWorkspace";
+
+export default function InfrastructureTopologyPage() {
+    return (
+        <Container className="py-8">
+            <TopologyWorkspace />
+        </Container>
+    );
+}
