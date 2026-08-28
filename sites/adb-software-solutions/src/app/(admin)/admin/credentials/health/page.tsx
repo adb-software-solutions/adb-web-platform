b5e15611974a3f499e060209a1aa80c5a27df01b
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2026-08-28 03:40:12 +0100

    feat: Add credential health page.

---
+++ sites/adb-software-solutions/src/app/(admin)/admin/credentials/health/page.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui";
+import { CredentialHealthWorkspace } from "./CredentialHealthWorkspace";
+
+export default function CredentialHealthPage() {
+    return (
+        <Container className="py-8">
+            <CredentialHealthWorkspace />
+        </Container>
+    );
+}
